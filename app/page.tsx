9b1457e798e5e5ca778784bf52bd23a132962c46
Cdreetz <cdreetz@gmail.com>
2024-08-04 11:04:39 -0500

    sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { Tldraw } from 'tldraw'
+import { useSyncDemo } from '@tldraw/sync'
 
 export default function App() {
+  const store = useSyncDemo({ roomId: 'myapp-abc123'})
+
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw />
+      <Tldraw store={store} />
     </div>
   )
 }
